af5625d89c64c63c03d8d93e472b42d6c6106f79
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-09-10 13:00:52 +0300

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
+import { RegistrationsModule } from './registrations/registrations.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, EventsModule, RegistrationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
