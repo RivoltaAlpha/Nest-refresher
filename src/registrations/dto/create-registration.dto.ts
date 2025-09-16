b17770b9ea6d9c8dcaa3a219a6b895658781286a
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-09-16 11:35:25 +0300

    registration

--- src/registrations/dto/create-registration.dto.ts
+++ src/registrations/dto/create-registration.dto.ts
@@ -1,1 +1,33 @@
-export class CreateRegistrationDto {}
+import { IsOptional, IsEnum, IsDate, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { paymentStatus } from '../entities/registration.entity';
+
+export class CreateRegistrationDto {
+    @ApiProperty()
+    @IsOptional()
+    event_id: number;
+
+    @ApiProperty()
+    @IsNumber()
+    user_id: number;
+
+    @ApiProperty()
+    @IsDate()
+    registration_date: Date;
+
+    @ApiProperty()
+    @IsEnum(paymentStatus)
+    payment_status: paymentStatus;
+    
+    @ApiProperty()
+    @IsNumber()
+    payment_amount: number;
+
+    @ApiProperty()
+    @IsDate()
+    created_at: Date;
+
+    @ApiProperty()
+    @IsDate()
+    updated_at: Date;
+}
--- src/registrations/entities/registration.entity.ts
+++ src/registrations/entities/registration.entity.ts
@@ -1,8 +1,44 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+
+export enum paymentStatus {
+    Pending = 'Pending',
+    Completed = 'Completed',
+    Failed = 'Failed',
+}
+
+@Entity('registrations')
 export class Registration {
-    registration_id
-event_id		// Reference to eventstable
-user_id		// Reference to userstable
-registration_date	
-payment_status	ENUM('Pending', 'Completed', 'Failed')	
-payment_amount
+  @PrimaryGeneratedColumn()
+  registration_id: number;
+
+  @Column()
+  event_id: number; // Reference to events table
+
+  @Column()
+  user_id: number; // Reference to users table
+
+  @CreateDateColumn()
+  registration_date: Date;
+
+  @Column({
+    type: 'enum',
+    enum: paymentStatus,
+  })
+  payment_status: paymentStatus;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  payment_amount: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
