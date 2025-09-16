a727e2b09d27d6398c7a18d681a3ad7610468742
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-09-16 12:10:44 +0300

    feedback entity

--- src/feedback/dto/create-feedback.dto.ts
+++ src/feedback/dto/create-feedback.dto.ts
@@ -1,1 +1,39 @@
-export class CreateFeedbackDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateFeedbackDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  feedback_id: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  event_id: number; // Reference to eventstable
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  user_id: number; // Reference to userstable
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsNumber()
+  rating: number;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  comments: string;
+  @ApiProperty()
+  @IsOptional()
+  @IsDate()
+  created_at: Date;
+}
--- src/feedback/entities/feedback.entity.ts
+++ src/feedback/entities/feedback.entity.ts
@@ -1,8 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity('feedbacks')
 export class Feedback {
-feedback_id	
-event_id		// Reference to eventstable
-user_id		// Reference to userstable
-rating	
-comments	
-created_at
+    @PrimaryGeneratedColumn()
+    feedback_id: number;
+
+    @Column()
+    event_id: number;
+    
+    @Column()
+    user_id: number;	// Reference to userstable
+
+    @Column({type: 'decimal', precision: 2})
+    rating: number;
+
+    @Column({type: 'varchar', length:255})
+    comments: string;
+    
+    @Column()
+    created_at: Date
 }
